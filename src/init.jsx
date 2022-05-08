d5fe0ef57bb3de6a6505ee8a7868d2fedf8e008c
Mikhail Lyalin <mc.lyalin@gmail.com>
2022-05-08 16:06:23 +0500

    create new cra-project

---
+++ src/init.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from './components/App.jsx';
+
+const init = () => {
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+};
+
+export default init;
